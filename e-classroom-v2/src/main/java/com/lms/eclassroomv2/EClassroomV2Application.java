7c96a2fb807b97522119aafdf4aff64124ebe01b
Lemur <danilo_jevtovic@hotmail.com>
2021-04-15 21:58:43 +0200

    first commit

---
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/EClassroomV2Application.java
@@ -0,0 +1,13 @@
+package com.lms.eclassroomv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EClassroomV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EClassroomV2Application.class, args);
+	}
+
+}
---
+++ e-classroom-v2/src/test/java/com/lms/eclassroomv2/EClassroomV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.eclassroomv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EClassroomV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
