aa34cc9c2f793a92915b4ba3e1b9463a0c95e240
lemur <danilo_jevtovic@hotmail.com>
2021-04-18 00:14:48 +0200

    crud servisi za kurs

--- e-classroom-v2/src/main/java/com/lms/eclassroomv2/services/CourseService.java
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/services/CourseService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lms.eclassroomv2.model.Course;
+import com.lms.eclassroomv2.model.dto.CourseDto;
 import com.lms.eclassroomv2.repository.CourseRepository;
 
 @Service
@@ -13,12 +14,41 @@ public class CourseService {
 
 	@Autowired
 	CourseRepository courseRepository;
-	
+
+	@Autowired
+	TeacherService teacherService;
+
 	public List<Course> getAllCourses() {
 		return courseRepository.findAll();
 	}
-	
+
 	public Course getCourseById(Long courseId) {
 		return courseRepository.findById(courseId).orElse(null);
+	}
+
+	public Course addCourse(CourseDto courseDto) {
+		Course course = new Course();
+		course.setName(courseDto.getName());
+		course.setDescription(courseDto.getDescription());
+		course.setDeleted(false);
+		course.setTeacher(teacherService.getTeacherById(courseDto.getTeacherId()));
+
+		return courseRepository.save(course);
+	}
+
+	public Course updateCourse(Long courseId, CourseDto courseDto) {
+		Course course = getCourseById(courseId);
+		course.setName(courseDto.getName());
+		course.setDescription(courseDto.getDescription());
+		course.setTeacher(teacherService.getTeacherById(courseDto.getTeacherId()));
+
+		return courseRepository.save(course);
+	}
+	
+	//logicko brisanje! - mozes da napravis kao arhiviranje kursa - pa samo da mu mjenjas taj status 
+	public void deleteCourse(Long courseId) {
+		Course course = getCourseById(courseId);
+		course.setDeleted(true);
+		courseRepository.save(course);
 	}
 }
