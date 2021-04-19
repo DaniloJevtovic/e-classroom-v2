c9237118d916922e5c0fa07bb4294c6cfc33e2a1
lemur <danilo_jevtovic@hotmail.com>
2021-04-19 14:29:49 +0200

    login i token dto klase

---
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/model/dto/TokenDto.java
@@ -0,0 +1,33 @@
+package com.lms.eclassroomv2.model.dto;
+
+public class TokenDto {
+	
+    private String accessToken;
+    private Long expiresIn;
+
+    public TokenDto() {
+        this.accessToken = null;
+        this.expiresIn = null;
+    }
+
+    public TokenDto(String accessToken, long expiresIn) {
+        this.accessToken = accessToken;
+        this.expiresIn = expiresIn;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+}
