47ec31452350ecb541a1b3224b9f26331ea2413d
lemur <danilo_jevtovic@hotmail.com>
2021-04-19 17:33:29 +0200

    prava pristupa

--- e-classroom-v2/src/main/java/com/lms/eclassroomv2/config/WebSecurityConfig.java
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/config/WebSecurityConfig.java
@@ -1,32 +1,74 @@
 package com.lms.eclassroomv2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.lms.eclassroomv2.security.TokenUtils;
+import com.lms.eclassroomv2.security.auth.RestAuthenticationEntryPoint;
+import com.lms.eclassroomv2.security.auth.TokenAuthenticationFilter;
+import com.lms.eclassroomv2.services.CustomUserDetailsService;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// TokenAuthenticationFilter ce ignorisati sve ispod navedene putanje
-		web.ignoring().antMatchers(HttpMethod.GET, "/**");
-		web.ignoring().antMatchers(HttpMethod.POST, "/**");
-		web.ignoring().antMatchers(HttpMethod.PUT, "/**");
-		web.ignoring().antMatchers(HttpMethod.DELETE, "/**");
-		
-		web.ignoring().antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "/favicon.ico", "/**/*.html",
-				"/**/*.css", "/**/*.js");
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+
+	@Autowired
+	private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Autowired
+	private TokenUtils tokenUtils;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+			.authorizeRequests().antMatchers("/auth/**").permitAll().antMatchers("/h2-console/**").permitAll()
+			.antMatchers("/api/foo").permitAll()
+			.anyRequest().authenticated().and()
+			.cors().and()
+			.addFilterBefore(new TokenAuthenticationFilter(tokenUtils, customUserDetailsService),
+						BasicAuthenticationFilter.class);
+		
+		http.csrf().disable();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(HttpMethod.POST, "/auth/login");
+		web.ignoring().antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "/favicon.ico", "/**/*.html",
+				"/**/*.css", "/**/*.js");
 	}
 
 }
