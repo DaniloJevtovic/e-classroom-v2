77363ca4f74cac12d4fdef17cc371fdcb611646c
lemur <danilo_jevtovic@hotmail.com>
2021-04-24 01:36:19 +0200

    servisi za dodavanje i ucitavanje svih materija + kontroler

---
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/controller/MaterialController.java
@@ -0,0 +1,44 @@
+package com.lms.eclassroomv2.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lms.eclassroomv2.model.Material;
+
+import com.lms.eclassroomv2.model.dto.MaterialDto;
+import com.lms.eclassroomv2.services.MaterialService;
+
+@RestController
+@RequestMapping(value = "api/materials")
+public class MaterialController {
+
+	@Autowired
+	MaterialService materialService;
+
+	@PreAuthorize("hasRole ('TEACHER')")
+	@GetMapping()
+	public List<Material> getAllMaterials() {
+		return materialService.getAllMaterials();
+	}
+
+	@PreAuthorize("hasRole ('ADMIN') or hasRole('TEACHER') or hasRole('STUDENT')")
+	@GetMapping(value = "/{materialId}")
+	public Material getMaterialById(@PathVariable Long materialId) {
+		return materialService.getMaterialById(materialId);
+	}
+
+	@PreAuthorize("hasRole ('TEACHER')")
+	@PostMapping
+	public Material addNewMaterial(@RequestBody MaterialDto materialDto) {
+		return materialService.newMaterial(materialDto);
+	}
+
+}
---
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/services/MaterialService.java
@@ -0,0 +1,38 @@
+package com.lms.eclassroomv2.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lms.eclassroomv2.model.Material;
+import com.lms.eclassroomv2.model.dto.MaterialDto;
+import com.lms.eclassroomv2.repository.MaterialRepository;
+
+@Service
+public class MaterialService {
+
+	@Autowired
+	MaterialRepository materialRepository;
+
+	@Autowired
+	CourseService courseService;
+
+	public List<Material> getAllMaterials() {
+		return materialRepository.findAll();
+	}
+
+	public Material getMaterialById(Long materialId) {
+		return materialRepository.findById(materialId).orElse(null);
+	}
+
+	public Material newMaterial(MaterialDto materialDto) {
+		Material material = new Material();
+		material.setName(materialDto.getName());
+		material.setDescription(materialDto.getDescription());
+		material.setCourse(courseService.getCourseById(materialDto.getCourseId()));
+
+		return materialRepository.save(material);
+	}
+
+}
