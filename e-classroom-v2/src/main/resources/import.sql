0bba2a66256024eddef1327e646e7a297bd5cc40
lemur <danilo_jevtovic@hotmail.com>
2021-05-11 15:36:07 +0200

    ubaceni podaci za kviz, pitanja i odgovore

--- e-classroom-v2/src/main/resources/import.sql
+++ e-classroom-v2/src/main/resources/import.sql
@@ -48,3 +48,43 @@ INSERT INTO courses (name, description, is_deleted, teacher_id, school_class_id)
 INSERT INTO materials (name, description, course_id) VALUES ('lekcija 1', 'opis lekcije 1', 1)
 INSERT INTO materials (name, description, course_id) VALUES ('lekcija 2', 'opis lekcije 2', 1)
 
+--kviz 1 - sa pitanjima i odgovorima
+INSERT INTO quizzes (name, instructions, duration, course_id) VALUES ('kviz 1', 'instrukcije za kviz 1', 15, 1)
+
+INSERT INTO questions (question, points, quiz_id) VALUES ('pit 1 kv1', 2, 1)
+INSERT INTO questions (question, points, quiz_id) VALUES ('pit 2 kv1', 3, 1)
+INSERT INTO questions (question, points, quiz_id) VALUES ('pit 3 kv1', 2, 1)
+
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg1 pit3', true, 3)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg2 pit3', false, 3)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg3 pit3', true, 3)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg1 pit1', true, 1)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg2 pit1', false, 1)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg1 pit2', true, 2)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg2 pit2', false, 2)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg3 pit2', true, 2)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg4 pit2', false, 2)
+
+--kviz 2 - sa pitanjima i odgovorima
+INSERT INTO quizzes (name, instructions, duration, course_id) VALUES ('kviz 2', 'instrukcije za kviz 2', 30, 1)
+
+INSERT INTO questions (question, points, quiz_id) VALUES ('pit1 kv2', 2, 2)
+INSERT INTO questions (question, points, quiz_id) VALUES ('pit2 kv2', 3, 2)
+INSERT INTO questions (question, points, quiz_id) VALUES ('pit3 kv2', 5, 2)
+
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg1 pit3', true, 6)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg2 pit3', false, 6)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg3 pit3', true, 6)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg1 pit1', true, 4)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg2 pit1', false, 4)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg1 pit2', true, 5)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg2 pit2', false, 5)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg3 pit2', true, 5)
+INSERT INTO answers (answer, correct, question_id) VALUES ('odg4 pit2', false, 5)
+
+
+
+
+
+
+
