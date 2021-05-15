02a6d76da8a156d432139d7b89883e9a0cf2ff14
lemur <danilo_jevtovic@hotmail.com>
2021-05-15 20:57:48 +0200

    dobavljanje primljenih i poslatih poruka

--- e-classroom-v2/src/main/java/com/lms/eclassroomv2/repository/MessageRepository.java
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/repository/MessageRepository.java
@@ -1,5 +1,6 @@
 package com.lms.eclassroomv2.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,8 @@ import com.lms.eclassroomv2.model.Message;
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
 	Optional<Message> findById(Long messageId);
+	
+	List<Message> findBySenderId(Long senderId);
+	
+	List<Message> findByReciverId(Long reciverId);
 }
