125c14ed4121d220fdac3f93f2982c814296d6b5
lemur <danilo_jevtovic@hotmail.com>
2021-05-16 21:52:14 +0200

    insertovani podaci o roditeljima

--- e-classroom-v2/src/main/resources/import.sql
+++ e-classroom-v2/src/main/resources/import.sql
@@ -2,17 +2,22 @@
 INSERT INTO authority (name) VALUES ('ROLE_ADMIN');
 insert INTO authority (name) VALUES ('ROLE_TEACHER');
 insert INTO authority (name) VALUES ('ROLE_STUDENT');
+insert INTO authority (name) VALUES ('ROLE_PARENT');
 
 INSERT INTO users (role, username, password, first_name, last_name, email, enabled, last_password_reset_date) VALUES ('admin','admin', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Direktor', 'Skole', 'direktor@gmail.com', true, '2021-04-15 21:58:58');
 --profesori
 INSERT INTO users (role, username, password, first_name, last_name, email, enabled, last_password_reset_date, subjects) VALUES ('teacher', 'profMat', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Profesor', 'Matematike', 'prof_mat@gmail.com', true, '2021-04-15 21:58:58', 'mat, inf');
 INSERT INTO users (role, username, password, first_name, last_name, email, enabled, last_password_reset_date, subjects) VALUES ('teacher', 'profSrp', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Profesor', 'Srpskog', 'prof_srp@gmail.com', true, '2021-04-15 21:58:58', 'srp');
+
 --ucenici
-INSERT INTO users (role, username, password, first_name, last_name, email, enabled, last_password_reset_date, student_class_id) VALUES ('student', 'ucPrviA', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Ucenik Ia', 'Prvog', 'uc1a_prvog@gmail.com', true, '2021-04-15 21:58:58', 1);
+INSERT INTO users (role, username, password, first_name, last_name, email, enabled, last_password_reset_date, student_class_id, st_parent_id) VALUES ('student', 'ucPrviA', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Ucenik Ia', 'Prvog', 'uc1a_prvog@gmail.com', true, '2021-04-15 21:58:58', 1, 8);
 INSERT INTO users (role, username, password, first_name, last_name, email, enabled, last_password_reset_date, student_class_id) VALUES ('student', 'ucPrviA2', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Ucenik Ia - 2 ', 'Prvog', 'uc1a2_prvog@gmail.com', true, '2021-04-15 21:58:58', 1);
 
 INSERT INTO users (role, username, password, first_name, last_name, email, enabled, last_password_reset_date, student_class_id) VALUES ('student', 'ucPrviB', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Ucenik IB', 'Prvog', 'uc1a_prvogB@gmail.com', true, '2021-04-15 21:58:58', 2);
 INSERT INTO users (role, username, password, first_name, last_name, email, enabled, last_password_reset_date, student_class_id) VALUES ('student', 'ucPrviB2', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Ucenik IB - 2 ', 'Prvog', 'uc1a2_prvogB@gmail.com', true, '2021-04-15 21:58:58', 2);
+
+--roditelji
+INSERT INTO users (role, username, password, first_name, last_name, email, enabled, last_password_reset_date) VALUES ('parent', 'rodPrviA', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Roditelj uc IA', 'Prvog A', 'rod_prvog@gmail.com', true, '2021-04-15 21:58:58');
 
 --dodjela uloga
 INSERT INTO user_authority (user_id, authority_id) VALUES (1, 1);
@@ -22,6 +27,7 @@ INSERT INTO user_authority (user_id, authority_id) VALUES (4, 3);
 INSERT INTO user_authority (user_id, authority_id) VALUES (5, 3);
 INSERT INTO user_authority (user_id, authority_id) VALUES (6, 3);
 INSERT INTO user_authority (user_id, authority_id) VALUES (7, 3);
+INSERT INTO user_authority (user_id, authority_id) VALUES (8, 4);
 
 --razredi -- I, II, III, IV
 insert into school_class (name, description) values ('I', 'prvi razred')
