0d3f362043dccdff81cb31f004fec464c2a9263d
lemur <danilo_jevtovic@hotmail.com>
2021-05-17 18:27:57 +0200

    dobavljanje korisnika na osnovu id-a

--- e-classroom-v2/src/main/java/com/lms/eclassroomv2/controller/UserController.java
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +15,20 @@ import com.lms.eclassroomv2.services.UserService;
 @RestController
 @RequestMapping("api/users")
 public class UserController {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@PreAuthorize("hasRole ('ADMIN')")
-	@GetMapping() 
+	@GetMapping()
 	public List<User> getAllUsers() {
 		return userService.getAllUsers();
+	}
+
+	@PreAuthorize("hasRole ('ADMIN') or hasRole('TEACHER') or hasRole('STUDENT') or hasRole('PARENT')")
+	@GetMapping("/{userId}")
+	public User getUserById(@PathVariable Long userId) {
+		return userService.getUserById(userId);
 	}
 
 }
