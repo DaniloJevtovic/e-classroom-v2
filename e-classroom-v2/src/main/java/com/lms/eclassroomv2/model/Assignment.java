8d3b9246ee60dc113b1b187875713f81961604cf
lemur <danilo_jevtovic@hotmail.com>
2021-06-02 10:48:44 +0200

    model zadatka za predmet

---
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/model/Assignment.java
@@ -0,0 +1,62 @@
+package com.lms.eclassroomv2.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "assignments")
+public class Assignment {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	private String instructions;
+
+	@ManyToOne
+	Course course;
+
+	public Assignment() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInstructions() {
+		return instructions;
+	}
+
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+}
