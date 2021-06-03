e7739bc12abcbe9ecf2ce621a1c9cce1bd41b965
lemur <danilo_jevtovic@hotmail.com>
2021-06-03 22:52:35 +0200

    dto zadatka

---
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/model/dto/AssignmentDto.java
@@ -0,0 +1,41 @@
+package com.lms.eclassroomv2.model.dto;
+
+public class AssignmentDto {
+
+	private String name;
+	private String description;
+	private int points;
+	private Long courseId;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+}
