a34656f245ff4c5f5ed68bae928f3af0aca80ec2
lemur <danilo_jevtovic@hotmail.com>
2021-06-05 11:20:20 +0200

    dohvacanje svih zadataka za predmet

--- e-classroom-v2/src/main/java/com/lms/eclassroomv2/repository/AssignmentRepository.java
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/repository/AssignmentRepository.java
@@ -1,9 +1,15 @@
 package com.lms.eclassroomv2.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lms.eclassroomv2.model.Assignment;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
 
+	Optional<Assignment> findById(Long id);
+	
+	List<Assignment> findByCourseId(Long courseId);
 }
