aba3a9b30f9553a550e893a67cd0f352b9f252fa
lemur <danilo_jevtovic@hotmail.com>
2021-07-04 19:58:55 +0200

    izmjena korisnickih podataka

--- e-classroom-v2/src/main/java/com/lms/eclassroomv2/services/UserService.java
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/services/UserService.java
@@ -1,11 +1,17 @@
 package com.lms.eclassroomv2.services;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.lms.eclassroomv2.model.User;
+import com.lms.eclassroomv2.model.dto.UpdateUserDto;
 import com.lms.eclassroomv2.repository.UserRepository;
 
 @Service
@@ -13,7 +19,10 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
 	public List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
@@ -28,6 +37,35 @@ public class UserService {
 
 	public User getUserByEmail(String email) {
 		return userRepository.findByEmail(email);
+	}
+
+	public ResponseEntity<?> updateUser(String username, UpdateUserDto updateUserDto) {
+		User user = getUserByUsername(username);
+
+		if (user == null) {
+			return new ResponseEntity<>("Ne postoji korisnik", HttpStatus.BAD_REQUEST);
+		}
+
+		if (updateUserDto.getNewPassword() != null) {
+			user.setPassword(passwordEncoder.encode(updateUserDto.getNewPassword()));
+		}
+
+		try {
+			user.setFirstName(updateUserDto.getFirstName());
+			user.setLastName(updateUserDto.getLastName());
+			user.setEmail(updateUserDto.getEmail());
+			userRepository.save(user);
+
+			Map<String, Object> map = new HashMap<>();
+			map.put("user", userRepository.save(user));
+			map.put("message", "podaci uspjesno azurirani");
+
+			return new ResponseEntity<>(map, HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return new ResponseEntity<>("Greska", HttpStatus.BAD_REQUEST);
+		}
 	}
 
 }
