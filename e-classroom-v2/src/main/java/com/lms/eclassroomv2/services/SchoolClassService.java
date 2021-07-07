139f2e6d3a611bb141a55000d078e9052cdf6491
lemur <danilo_jevtovic@hotmail.com>
2021-07-07 20:26:49 +0200

    sprjecavanje unosa razreda sa istim imenom

--- e-classroom-v2/src/main/java/com/lms/eclassroomv2/services/SchoolClassService.java
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/services/SchoolClassService.java
@@ -3,6 +3,8 @@ package com.lms.eclassroomv2.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.lms.eclassroomv2.model.SchoolClass;
@@ -22,15 +24,46 @@ public class SchoolClassService {
 		return schoolClassRepository.findById(schoolClassId).orElse(null);
 	}
 
-	public SchoolClass newSchoolClass(SchoolClass scClass) {
-		return schoolClassRepository.save(scClass);
+	public SchoolClass getSchoolClassByName(String scName) {
+		return schoolClassRepository.findByName(scName);
 	}
 
-	public SchoolClass editSchoolClass(Long scClassId, SchoolClass scClass) {
-		SchoolClass schoolClass = getSchoolClassById(scClassId);
-		schoolClass.setName(scClass.getName());
-		schoolClass.setDescription(scClass.getDescription());
-		return schoolClassRepository.save(schoolClass);
+	public ResponseEntity<?> newSchoolClass(SchoolClass scClass) {
+
+		try {
+			SchoolClass sc = getSchoolClassByName(scClass.getName());
+
+			// provjeravam da li postoji vec kreiran taj razred kako ne bi bila 2 sa istim
+			// imenom
+			if (sc != null) {
+				return new ResponseEntity<>("Postoji vec kreiran taj razred", HttpStatus.BAD_REQUEST);
+			}
+
+			schoolClassRepository.save(scClass);
+
+			return new ResponseEntity<>("Razred uspjesno kreiran", HttpStatus.CREATED);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Nije moguce kreirati razred", HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	public ResponseEntity<?> editSchoolClass(Long scClassId, SchoolClass scClass) {
+		try {
+			SchoolClass schoolClass = getSchoolClassById(scClassId);
+			schoolClass.setName(scClass.getName());
+			schoolClass.setDescription(scClass.getDescription());
+			schoolClassRepository.save(schoolClass);
+
+			return new ResponseEntity<>("Razred uspjesno azuriran", HttpStatus.OK);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Nije moguce azurirati razred", HttpStatus.BAD_REQUEST);
+		}
+
 	}
 
 }
