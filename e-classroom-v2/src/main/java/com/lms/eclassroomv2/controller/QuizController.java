7d5f185876de2e8fc7fe17331c205b62bf043646
lemur <danilo_jevtovic@hotmail.com>
2021-07-08 20:42:29 +0200

    response entity za kviz

--- e-classroom-v2/src/main/java/com/lms/eclassroomv2/controller/QuizController.java
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/controller/QuizController.java
@@ -3,6 +3,7 @@ package com.lms.eclassroomv2.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,7 +46,7 @@ public class QuizController {
 
 	@PreAuthorize("hasRole ('TEACHER')")
 	@PostMapping
-	public Quiz addNewQuiz(@RequestBody QuizDto quizDto) {
+	public ResponseEntity<?> addNewQuiz(@RequestBody QuizDto quizDto) {
 		return quizService.newQuiz(quizDto);
 	}
 
--- e-classroom-v2/src/main/java/com/lms/eclassroomv2/services/QuizService.java
+++ e-classroom-v2/src/main/java/com/lms/eclassroomv2/services/QuizService.java
@@ -3,6 +3,8 @@ package com.lms.eclassroomv2.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.lms.eclassroomv2.model.Quiz;
@@ -31,15 +33,24 @@ public class QuizService {
 		return quizRepository.findByCourseId(courseId);
 	}
 
-	public Quiz newQuiz(QuizDto quizDto) {
-		Quiz quiz = new Quiz();
-		quiz.setName(quizDto.getName());
-		quiz.setInstructions(quizDto.getInstructions());
-		quiz.setDuration(quizDto.getDuration());
-		quiz.setCourse(courseService.getCourseById(quizDto.getCourseId()));
-		quiz.setQuizStatus(QuizStatus.INACTIVE);	//da ne bi odma ucenici mogli da ga rjesavaju kad se kreira kviz
+	public ResponseEntity<?> newQuiz(QuizDto quizDto) {
+		try {
+			Quiz quiz = new Quiz();
+			quiz.setName(quizDto.getName());
+			quiz.setInstructions(quizDto.getInstructions());
+			quiz.setDuration(quizDto.getDuration());
+			quiz.setCourse(courseService.getCourseById(quizDto.getCourseId()));
+			quiz.setQuizStatus(QuizStatus.INACTIVE); // da ne bi odma ucenici mogli da ga rjesavaju kad se kreira kviz
 
-		return quizRepository.save(quiz);
+			quizRepository.save(quiz);
+			return new ResponseEntity<>("Kviz uspjesno kreiran", HttpStatus.CREATED);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return new ResponseEntity<>("Greska u kreiranju kviza", HttpStatus.BAD_REQUEST);
+		}
+
 	}
 
 	public Quiz updateQuiz(Long quizId, QuizDto quizDto) {
